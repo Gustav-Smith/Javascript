a6a0e847eb158fc0ec2af7c34a14d4b9a2c28995
Gustav-Smith <gustavj3an@gmail.com>
2025-05-01 19:25:07 -0300

    feat: initialize lifehub-api with authentication and task routes
    - Added package.json for project dependencies and scripts.
    - Implemented authentication routes (register and login) in authRoutes.js.
    - Created taskRoutes.js with a sample route for tasks.
    - Set up server.js to connect to MongoDB and use the defined routes.

--- node/aula01/mod1.js
+++
@@ -1,7 +0,0 @@
-class Pessoa {
-  constructor(nome) {
-    this.nome = nome;
-  }
-}
-
-exports.Pessoa = Pessoa; // Exportando a classe Pessoa
--- node/aula02/app.js
+++
@@ -1,4 +0,0 @@
-const caramelo = require('./mod');
-
-const cachorro = new caramelo('Dog');
-cachorro.latir();
